61a8ee75c74a9ea07364ffa2b451224c97bcb7a0
Sruba Mohapatra <sruba.mohapatra@publicissapient.com>
2023-10-26 17:48:51 +0530

    Added assignment 1 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { Assignment1Component } from './assignment1/assignment1.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'assignment1', component: Assignment1Component },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/assignment1/assignment1.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Assignment1Component } from './assignment1/assignment1.component';
+
+
+
+@NgModule({
+  declarations: [
+    Assignment1Component
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class Assignment1Module { }
